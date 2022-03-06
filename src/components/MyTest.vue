3e19b66f953404b1ecbdb27630cce81d3f51669b
fwhdzh <1064039735@qq.com>
2022-03-06 12:27:09 +0800

    初始脚手架

---
+++ src/components/MyTest.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        p
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyTest',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style>
+
+</style>
+
